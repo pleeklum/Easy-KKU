d9569fbfdfc4c991c0608b410ab86a060bcaaad8
touchily <teeratouch@kkumail.com>
2016-11-12 15:26:44 +0700

    photo

--- app/src/main/java/com/pleeklum/easykku/SignUpActivity.java
+++ app/src/main/java/com/pleeklum/easykku/SignUpActivity.java
@@ -1,13 +1,92 @@
 package com.pleeklum.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, phoneEditText,
+            userEditText, passwordEditText;
+    private ImageView imageView;
+    private Button button;
+    private String nameString, phoneString, userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        phoneEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+
+        //SignUp Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From Edit Text
+                nameString = nameEditText.getText().toString().trim();
+                phoneString = phoneEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") || phoneString.equals("") ||
+                        userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+                    Log.d("12novV1", "Have Space");
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.bird48,
+                            "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่องคะ");
+                    myAlert.myDialog();
+                }
+
+
+            }   // onClick
+        });
+
+
+        //Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรดเลือกแอพดูภาพ"), 0);
+
+            }
+        });
+
+
+
+    }   // Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == 0) && (requestCode == RESULT_OK)) {
+
+            Log.d("12novV1", "Result OK");
+
+
+        }//if
+
+
+    }//OnActivity
 }
+// Main Class
